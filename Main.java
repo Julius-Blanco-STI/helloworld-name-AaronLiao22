5aeab26e6cd16db9d7196c20b589af5a96a78789
Steve <58575823+AaronLiao22@users.noreply.github.com>
2019-12-08 14:15:54 +0800

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,26 +1,18 @@
-
 package main;
 import java.util.Scanner;
-
 public class Main {
-
-    
+  
     public static void main(String[] args) {
         Scanner scnr = new Scanner (System.in);
-        
         System.out.println("Hello World!");
         System.out.println("What's your name?");
-        String name = scnr.nextLine();
-                
+        String name = scnr.nextLine();          
         System.out.println("How Old are you?");
-        int age =  scnr.nextInt();
-                
+        int age =  scnr.nextInt();             
         System.out.println("What's your address?");
-        String adr = scnr.next();
-        
+        String adr = scnr.next();      
         System.out.println("Hi" + " " + name + "!");
         System.out.println("You are " + age + " years old");
         System.out.println("You live at " + adr);
-    }
-    
+    } 
 }
